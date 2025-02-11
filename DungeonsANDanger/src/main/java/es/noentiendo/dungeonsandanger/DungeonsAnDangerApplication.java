7954413483b7702d01b7a6ddaa72b29ec2bdcba9
Jaime <67060224+Mineja2017@users.noreply.github.com>
2025-02-11 09:31:24 +0100

    Create Project and base files

---
+++ DungeonsANDanger/src/main/java/es/noentiendo/dungeonsandanger/DungeonsAnDangerApplication.java
@@ -0,0 +1,14 @@
+package es.noentiendo.dungeonsandanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAnDangerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DungeonsAnDangerApplication.class, args);
+    }
+
+}
---
+++ DungeonsANDanger/src/test/java/es/noentiendo/dungeonsandanger/DungeonsAnDangerApplicationTests.java
@@ -0,0 +1,14 @@
+package es.noentiendo.dungeonsandanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsAnDangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
