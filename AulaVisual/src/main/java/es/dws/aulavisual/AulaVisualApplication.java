a06e8a3aabac86148d99e8de5c8c1dccb7bed78d
Mineja2017 <67060224+Mineja2017@users.noreply.github.com>
2025-02-11 18:47:32 +0100

    Add Fiist File of project

---
+++ AulaVisual/src/main/java/es/dws/aulavisual/AulaVisualApplication.java
@@ -0,0 +1,13 @@
+package es.dws.aulavisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaVisualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaVisualApplication.class, args);
+    }
+
+}
