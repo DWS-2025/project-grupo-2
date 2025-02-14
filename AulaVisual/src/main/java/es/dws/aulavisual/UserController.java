8666eeee47594ad7bd78f2fbaff175bf0e5a8666
Mineja2017 <67060224+Mineja2017@users.noreply.github.com>
2025-02-14 15:54:28 +0100

    UserUpdateFeature
    added user updates

--- AulaVisual/src/main/java/es/dws/aulavisual/UserController.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/UserController.java
@@ -5,10 +5,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import es.dws.aulavisual.users.UserManager;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Files;
 
@@ -58,6 +60,48 @@ public class UserController {
 
         userManager.addUser(name, surname, username, password, 2);
         return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletResponse response) {
+
+        Cookie cookie = new Cookie("userId", "");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+
+    @PostMapping("/profile/update")
+    public String updateUser(@RequestParam String username, @RequestParam String prevPassword, @RequestParam String newPassword, MultipartFile image, @CookieValue("userId") String userId) {
+
+        if(!username.isEmpty()){
+
+            if(userManager.updateUsername(Long.parseLong(userId), username)){
+
+                return "redirect:/login";
+            }else{
+
+                return "redirect:/profile/error";
+            }
+        }
+
+        if(!newPassword.isEmpty()){
+
+            if(userManager.updatePassword(Long.parseLong(userId), prevPassword, newPassword)){
+
+                return "redirect:/login";
+            }else{
+
+                return "redirect:/profile/error";
+            }
+        }
+
+        if(!image.isEmpty()){
+
+
+        }
+
+        return "/profile";
     }
 
     @GetMapping("/user_pfp{userId}")
