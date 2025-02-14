ec23f252f2f35478cc5178f1b3abe1a5c5311751
Mineja2017 <67060224+Mineja2017@users.noreply.github.com>
2025-02-14 15:12:21 +0100

    Fix broken strartup

--- AulaVisual/src/main/java/es/dws/aulavisual/Paths.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/Paths.java
@@ -2,6 +2,6 @@ package es.dws.aulavisual;
 
 public class Paths {
 
-    public static final String CURRENTUSERIDPATH = "src/main/files/users/currentUserId.txt";
-    public static final String USERSMAPPATH = "src/main/files/users/userMap.txt";
+    public static final String CURRENTUSERIDPATH = "files/users/currentUserId.txt";
+    public static final String USERSMAPPATH = "files/users/userMap.txt";
 }
