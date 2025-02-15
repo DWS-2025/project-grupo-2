e728b268ed73f619f20c64feef85ccca983c7f83
carlagmezc <carlagomezcabanillas@gmail.com>
2025-02-15 12:33:34 +0100

    Coment

--- AulaVisual/src/main/java/es/dws/aulavisual/UserController.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
 
-        return "login";
+        return "login"; /* return login template */
     }
 
     @PostMapping("/login")
