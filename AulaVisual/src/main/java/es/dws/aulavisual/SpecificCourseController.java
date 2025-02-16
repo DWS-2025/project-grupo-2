91fa36f498623fb39907ca0e1140d1ee703b6b46
Víctor <164388000+Gransuti44@users.noreply.github.com>
2025-02-16 14:09:25 +0100

    Uptade
    a

--- AulaVisual/src/main/java/es/dws/aulavisual/SpecificCourseController.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/SpecificCourseController.java
@@ -1,11 +1,15 @@
 package es.dws.aulavisual;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import es.dws.aulavisual.courses.CourseManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import es.dws.aulavisual.courses.CourseManager;
 
+
+@Controller
 public class SpecificCourseController {
+
     private final CourseManager courseManager;
     public SpecificCourseController(CourseManager courseManager) {
 
@@ -15,13 +19,6 @@ public class SpecificCourseController {
     @GetMapping("/course")
     public String course(Model model) {
 
-        model.addAttribute("name", courseManager.getCourses());
-        return "course";
-    }
-
-    @PostMapping ("/course")
-    public String showCourse(Model model) {
-
-        return "course";
+        return "courses/course";
     }
 }
--- AulaVisual/src/main/java/es/dws/aulavisual/courses/CourseManager.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/courses/CourseManager.java
@@ -1,8 +1,11 @@
 package es.dws.aulavisual.courses;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CourseManager {
 
     private List <Course> courses;
