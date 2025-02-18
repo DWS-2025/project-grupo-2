ef704471f6111522e569c9faeb84bfd166f7751d
Jaime <67060224+Mineja2017@users.noreply.github.com>
2025-02-18 11:35:45 +0100

    Merge branch 'main' into Create-My-Courses

--- AulaVisual/src/main/java/es/dws/aulavisual/MainViewController.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/MainViewController.java
@@ -1,14 +1,33 @@
 package es.dws.aulavisual;
 
+import es.dws.aulavisual.users.User;
+import es.dws.aulavisual.users.UserManager;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainViewController {
 
-    @GetMapping("/")
-    public String index() {
+    private final UserManager userManager;
 
+    public MainViewController(UserManager userManager) {
+        this.userManager = userManager;
+    }
+
+    @GetMapping("/")
+    public String index(@CookieValue(value = "userId", defaultValue = "") String userId, Model model) {
+
+        if (!userId.isEmpty()){
+            model.addAttribute("user", true);
+            model.addAttribute("userId", Long.parseLong(userId));
+            User user = userManager.getUser(Long.parseLong(userId));
+            model.addAttribute("userName", user.getUserName());
+        }else{
+
+            model.addAttribute("user", false);
+        }
         return "index";
     }
 
