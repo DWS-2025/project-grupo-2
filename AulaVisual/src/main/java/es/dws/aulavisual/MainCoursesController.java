dcf0d3bcfd7f93e98fa03f113d32e842f4e69c4c
Beatriz <beatriz.sanz.granados@gmail.com>
2025-02-20 11:38:54 +0100

    Add coursesviewPage.css stylesheet
    Styles of coursesview.html

--- AulaVisual/src/main/java/es/dws/aulavisual/MainCoursesController.java
+++ AulaVisual/src/main/java/es/dws/aulavisual/MainCoursesController.java
@@ -1,10 +1,14 @@
 package es.dws.aulavisual;
 
 import es.dws.aulavisual.courses.Course;
+import es.dws.aulavisual.courses.CourseManager;
+import es.dws.aulavisual.users.User;
+import es.dws.aulavisual.users.UserManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,16 +17,33 @@ import java.util.List;
 @Controller
 public class MainCoursesController {
 
+    private final CourseManager courseManager;
+    private final UserManager userManager;
+
+    public MainCoursesController(CourseManager courseManager, UserManager userManager) {
+        this.courseManager = courseManager;
+        this.userManager = userManager;
+    }
+
     @GetMapping("/coursesview")
     public String coursesview(Model model, @CookieValue(value = "userId", defaultValue = "") String userId) {
 
-        Course course = new Course(0, "test", "test1", null);
-        Course course2 = new Course(0, "test2", "test2", null);
-        Course course3 = new Course(0, "test3", "test3", null);
-        List <Course> courses = new ArrayList <>();
-        courses.add(course);
-        courses.add(course2);
-        courses.add(course3);
+        User user = userManager.getUser(Long.parseLong(userId));
+        if(user == null){
+            return "redirect:/";
+        }
+        List <Course> courses = courseManager.getCourses();
+
+        List <Long> usersInCourse;
+        for (Course course : courses) {
+
+            usersInCourse = course.getUserIds();
+            if(usersInCourse.contains(Long.parseLong(userId))){
+
+
+            }
+        }
+
         model.addAttribute("courses", courses);
         return "courses/coursesview";
     }
